074fc7f8d02b333353ea3fe44607f84a10ed9003
Liudimilla <42663858+Liudimilla@users.noreply.github.com>
2021-07-07 14:57:15 -0300

    Básico II

---
+++ DadosAluno.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class DadosAluno {
+    public static void main(String[] args) {
+        Scanner a = new Scanner (System.in);
+        long ra;
+        String nome, curso = null, faculdade;
+        nome = "Liudimilla";
+        ra =1234567L;
+        curso = "Ciencias da Computaçao";
+
+        System.out.println("Curso: "+curso+ "\nNome: "+nome+"\tRa: "+ra+"\t\tCurso: "+curso);
+
+
+    }
+}
---
+++ DadosFuncionario.java
@@ -0,0 +1,11 @@
+public class DadosFuncionario {
+    public static void main(String[] args) {
+        float salario = 20000.0f;
+        String nome = "Liudimilla";
+        String cargo = "Analista de sistema";
+        String empresa ="Fanonymodas";
+
+        System.out.println(" Empresa: "+empresa+"\n Nome: "+nome+"\n Cargo: "+cargo+"\tSalario: "+salario);
+
+    }
+}
